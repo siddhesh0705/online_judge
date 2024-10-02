e2055b4cbfff1154e33723c5fda966d1de44b128
siddhesh <siddheshsangale0705@gmail.com>
2024-10-02 09:37:13 +0530

    db.define error solved

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
-const db = require('../config/db.config.js');
+const sequelize = require('../config/sequelize');
 
-const User = db.define('User', {
+const User = sequelize.define('User', {
     user_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
